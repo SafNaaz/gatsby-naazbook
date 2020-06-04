17c21388ee13fda29d91c4bf6a0527abe163aed2
SAFNAS <iamsafnas@gmail.com>
2020-06-04 21:39:13 +0530

    added a nav menu and services page and incorporated in site

--- src/components/layout.js
+++ src/components/layout.js
@@ -12,6 +12,8 @@ import { useStaticQuery, graphql } from "gatsby"
 import Header from "./header"
 import "./layout.css"
 
+import Menu from "./menu"
+
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
@@ -26,6 +28,7 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
+      <Menu />
       <div
         style={{
           margin: `0 auto`,
---
+++ src/pages/services.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+function Services() {
+  return (
+    <Layout>
+      <SEO title="Services" />
+      <h1>Services</h1>
+      <p>Programming..</p>
+      <p>Just about eveything related to Tech..</p>
+    </Layout>
+  )
+}
+
+export default Services
