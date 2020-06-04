0fb724607591ccfb777a162c14688edbc2cb51c3
SAFNAS <iamsafnas@gmail.com>
2020-06-04 21:23:53 +0530

    created an about page

---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+function AboutPage() {
+  return (
+    <Layout>
+      <SEO title="About" />
+      <h1>About Us</h1>
+      <p>iamsafnas@gmail.com</p>
+    </Layout>
+  )
+}
+
+export default AboutPage
